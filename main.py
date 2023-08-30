cda6157ee77f41a88c774a87c6c9ca78d0473b49
Chardy Fernando <ccfernando@crc.pshs.edu.ph>
2023-08-30 08:33:53 +0800

    Added Main.py with examples

---
+++ main.py
@@ -0,0 +1,25 @@
+
+# Python allows a variable to handle every data type available
+string_datatype = 'ha'
+int_datatype = 3
+float_datatype = 2.5
+boolean_datatype = False
+
+#What will be the outputs of the following?
+
+#1.
+print(string_datatype * int_datatype)
+
+#2.
+#print(string_datatype * float_datatype)
+
+#3.
+#print(string_datatype * boolean_datatype)
+
+#4.
+#print(int_datatype * float_datatype)
+
+# this_what = int_datatype * float_datatype
+# print(type(this_what))
+
+
